1a18eba677ceb25ad1aef5e340402f5352b2d7bc
Bryce B <bryce.boddy@gmail.com>
2020-06-26 07:34:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -15,9 +15,5 @@ class InteractiveRecord
     end
   end
   
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
+  def initialize
 end
