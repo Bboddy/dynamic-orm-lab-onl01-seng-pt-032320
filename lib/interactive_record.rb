57cea83eecd3dab2843636b9cf0f99ee62acc142
Bryce B <bryce.boddy@gmail.com>
2020-06-26 07:25:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,22 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def self.table_name 
+    "#{self.to_s.downcase}s"
+  end
   
+  def self.column_names
+    sql = "pragma table_info('#{table_name}')"
+
+    names = DB[:conn].execute(sql)
+    names.map do |row|
+      row["name"]
+    end
+  end
+  
+  def initialize(name, grade, id)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 end
